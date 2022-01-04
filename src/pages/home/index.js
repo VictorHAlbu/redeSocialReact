9eb0e873e8f11e1f6852ebb066e2e5d220a4ce92
VictorHAlbu <vh12albuquerque@gmail.com>
2022-01-03 21:43:58 -0300

    criando a estrutura home

---
+++ src/pages/home/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './style.css'
+
+function Home() {
+  return (
+    <div>
+      <header className="header">
+        <div className="toolbar">
+          <div>
+            <span>Conecta Dev</span>
+          </div>
+          <div>
+            <button>Novo Post</button>
+            <span>img1</span>
+            <span>img2</span>
+          </div>
+        </div>
+      </header>
+      <main className="main">
+        <div className="navbar">Navbar</div>
+        <div className="feed">Feed</div>
+      </main>
+    </div>
+  )
+}
+
+export default Home;
