0fec4b5edf23d46238619e681b5fc9288b61adf8
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2022-01-05 08:59:55 -0300

    incon feather

--- src/pages/home/componentes/Header.js
+++ src/pages/home/componentes/Header.js
@@ -3,16 +3,39 @@ import {Button} from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import SvgIcon from '@material-ui/core/Toolbar';
+import { Bell } from 'react-feather';
 
 const useStyles = makeStyles({
-
+  appBar:{
+    boxShadow: 'none',
+  },
+  img:{
+    maxHeight: 55,
+  },
+  grow:{
+    flexGrow: 1
+  }
 
 })
 
 function Header() {
+
+    const classes = useStyles();
+
   return (
-    <AppBar position='fixed' color='inherit'>
+    <AppBar position='fixed' color='inherit'className={classes.appBar}>
         <Toolbar>
+          <img src='/images/logo.png' alt="logo" className={classes.img} />
+          <div className={classes.grow}></div>
+          <div>
+            <Button variant='contained' color='primary'> 
+              Novo Post
+            </Button>
+            <SvgIcon>
+              <Bell></Bell>
+            </SvgIcon>
+          </div>
           {/* <div>
             <a href='/'>Conecta Dev</a>
             <input type='text'></input>
