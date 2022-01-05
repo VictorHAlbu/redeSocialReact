27000b771007c00b8e6a597c306577baec863bd6
VictorHAlbu <vh12albuquerque@gmail.com>
2022-01-05 08:23:39 -0300

    Aplicando style material Ui na home e header

--- src/pages/home/componentes/Header.js
+++ src/pages/home/componentes/Header.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import {Button} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
+const useStyles = makeStyles({
+
+
+})
 
 function Header() {
   return (
-    <header className="header">
-        <div className="toolbar">
-          <div>
+    <AppBar position='fixed' color='inherit'>
+        <Toolbar>
+          {/* <div>
             <a href='/'>Conecta Dev</a>
+            <input type='text'></input>
           </div>
           <div>
           <Button color='primary' variant="contained"> Novo Post</Button>;
             <span>img1</span>
             <span>img2</span>
-          </div>
-        </div>
-      </header>
+          </div> */}
+        </Toolbar>
+    </AppBar>
   )
 }
 
