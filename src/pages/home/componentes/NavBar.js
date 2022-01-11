cdf81c0f2b1edbeacf0ba304c904c67eb68fa3cf
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2022-01-11 08:40:21 -0300

    navbar

--- src/pages/home/componentes/NavBar.js
+++ src/pages/home/componentes/NavBar.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import Paper from '@material-ui/core/Paper';
+
+
+
+const useStyles = makeStyles({
+  root:{
+    padding: 16,
+    width: 275
+  }
+
+});
+
 
 function NavBar() {
+ 
+  const classes = useStyles();
+
   return(
-    <div>
+    <Paper className={classes.root}>
       NavBar
-    </div>
+    </Paper>
   )
 }
 
