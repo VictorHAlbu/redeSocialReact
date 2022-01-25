0a9b197c81cb6ae77e5bb61ffd3e4439480748f8
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2022-01-25 10:48:37 -0300

    post card - passando props para componente PostCar

--- src/pages/home/componentes/Feed.js
+++ src/pages/home/componentes/Feed.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import PostCard from '../../../componentes/PostCard'
+
+const useStyles = makeStyles((theme) => ({
+  root:{
+
+  }
+}))
+
+const posts = [
+  {id: 1, description: 'teste 1'},
+  {id: 2, description: 'teste 2'}
+
+] 
+
 
 function Feed() {
   return(
     <div>
-      Feed
+      {
+        posts.map(post =>(
+          <PostCard key={post.id} post={post} />
+        ))
+      }
     </div>
   )
 }
