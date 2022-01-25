e50274c63d9aa3f245299f45c8fbb30ae3c34525
VictorHAlbu <vh12albuquerque@gmail.com>
2022-01-25 10:17:57 -0300

    Button e list

--- src/pages/home/componentes/NavBar.js
+++ src/pages/home/componentes/NavBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import ListSubheader from '@material-ui/core/ListSubheader';
 
 
 
@@ -8,9 +10,22 @@ const useStyles = makeStyles({
   root:{
     padding: 16,
     width: 275
+  },
+  button:{
+    width: "100%"
   }
 
 });
+
+const tags = [
+  {id: 1, name: 'ractjs'},
+  {id: 2, name: 'javascript'},
+  {id: 3, name: 'dotnet'},
+  {id: 1, name: 'php'},
+  {id: 1, name: 'materialdesing'},
+  {id: 1, name: 'webdev'}
+
+]
 
 
 function NavBar() {
@@ -19,7 +34,8 @@ function NavBar() {
 
   return(
     <Paper className={classes.root}>
-      NavBar
+      <Button variant="outlined" color="secondary" className={classes.button}>Registrar Grátis</Button>
+      <ListSubheader>{'Tags em Alta'}</ListSubheader>
     </Paper>
   )
 }
